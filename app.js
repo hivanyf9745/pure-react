eb3d2ee68acdd2cab72c3a1dee40c2107ebca596
Ivan Huang <yifanhuang.Ivan@hotmail.com>
2022-06-17 21:01:55 -0400

    adding starting project

---
+++ app.js
@@ -0,0 +1,37 @@
+const Person = props => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', { key: 'person-child1' }, props.name),
+    React.createElement('p', { key: 'person-child2' }, props.occupation),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement(
+      'h1',
+      { className: 'title', key: 'app-child1' },
+      'React IS rendered'
+    ),
+    React.createElement(
+      Person,
+      { name: 'Yihua', occupation: 'instructor', key: 'app-child2' },
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: 'Andrei', occupation: 'Lead instructor', key: 'app-child3' },
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: 'Emily', occupation: 'Teaching Assistant', key: 'app-child4' },
+      null
+    ),
+  ]);
+};
+
+// ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
